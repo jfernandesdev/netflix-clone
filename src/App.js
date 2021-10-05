8b903314d187fbfc4dcc9fd8d8a60896125bd917
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-05 16:35:27 -0300

    movie list component creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 import api from './service/api'
 
+import MovieRow from './components/MovieRow';
+
+import './style/app.module.scss';
 
 export default function App() {
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
-      //get list all
       let list = await api.getHomeList();
       setMovieList(list);
     }
@@ -16,6 +18,12 @@ export default function App() {
   }, []);
 
   return (
-    <h1>Hello World!</h1>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
   );
 }
---
+++ src/components/MovieRow/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './styles.module.scss';
+
+export default ({ title, items }) => {
+  return (
+    <div className={style.listContainer}>
+      <h2>{title}</h2>
+      <div className={style.listRow}>
+        <div className={style.list}>
+          {items.results.length > 0 && items.results.map((item, key) => (
+            <div className={style.item} key={key}>
+              <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
